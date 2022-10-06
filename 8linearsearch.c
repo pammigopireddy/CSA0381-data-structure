8ad46ee40a1eee721b5a5375e2899ce221fd1fe4
pammigopireddy <113510690+pammigopireddy@users.noreply.github.com>
2022-10-06 09:37:25 +0530

    Add files via upload

---
+++ 8linearsearch.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+	int num;
+	int i,keynum,found=0;
+	printf("enter the number of elements ");
+	scanf("%d",&num);
+	int array[num];
+	printf("enter the elements one by one\n");
+	for(i=0;i<num;i++)
+	{
+		scanf("%d",&array[i]);
+	}
+	printf("enter the element to be searched");
+	scanf("%d",&keynum);
+	for(i=0;i<num;i++)
+	{
+		if(keynum==array[i])
+		{
+			found=1;
+			break;
+		}
+	}
+	if(found==1)
+	{
+	printf("element is present in the array at position %d",i+1);
+    }
+	else
+	{
+		printf("element is not present in the array\n");
+		}	
+	
+	
+}
